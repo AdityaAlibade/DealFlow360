f90e5a001fef8d2fcb64da9e54dd2c9e45789b22
AdityaAlibade <alibadeaditya@gmail.com>
2026-09-05 12:22:08 +0530

    Remove all files

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-// TODO: General helper functions
-export const calculateTotal = (items = []) => {
-  // TODO: Calculate line item totals
-  return items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-};
-
-export const classNames = (...classes) => {
-  return classes.filter(Boolean).join(' ');
-};
